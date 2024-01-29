7ce09c98ff3713e24e9e02fd3975d8be2573f8fd
Muhammed Hamza <hamzamuhammed404@gmail.com>
2024-01-29 18:21:46 +0500

    Adding New File

---
+++ week8/src/test/java/org/OpenHrm/config/SampleFeatureTest.java
@@ -0,0 +1,14 @@
+package UtilitiesFactory;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
